ded5595e5587f5b74a14481a12e802ef5f127a83
McCambley <jakemccamb16@gmail.com>
2021-04-19 13:46:25 -0400

    Refactor: removes redundant JS code

--- scripts/index.js
+++ scripts/index.js
@@ -22,7 +22,7 @@ const popupImageTitle = document.querySelector(
 );
 const popupImageLink = document.querySelector(".popup__input_role_image-link");
 
-// Image preview identifier
+// Image preview identifiers
 const imagePreview = document.querySelector(".popup_role_image");
 const closePreviewButton = document.querySelector(".popup__close_role_image");
 const popupImage = document.querySelector(".popup__image");
@@ -61,10 +61,13 @@ const initialCards = [
 ];
 
 initialCards.forEach((place) => {
+  // Populate page with initial places object
   addPlace(place.name, place.link);
 });
 
 // functions
+
+// Creates an object containing content of profile
 function updateProfile() {
   profile = {
     name: profileName.textContent,
@@ -72,96 +75,104 @@ function updateProfile() {
   };
 }
 
+// Open profile editor
 function editProfile() {
   updateProfile();
+  // Populate inputs values with current profile object values
   popupName.value = profile.name;
   popupTitle.value = profile.title;
+  // Open popup with updated values
   profileEditor.classList.add("popup_opened");
-
+  // Listen for escape button
   body.addEventListener("keyup", function escOut(e) {
     if (e.key === "Escape") {
-      closeEditor();
+      closePopup();
     }
   });
 }
 
+// Open new place popup
 function openPlaceAdder() {
   newPlaceAdder.classList.add("popup_opened");
+  // Refresh input values with blank strings
   popupImageTitle.value = "";
   popupImageLink.value = "";
-
+  // Listen for escape button
   body.addEventListener("keyup", function escOut(e) {
     if (e.key === "Escape") {
-      closeAdder();
+      closePopup();
     }
   });
 }
 
+// Add new place to places container
 function addPlace(title, link) {
+  // Target and clone template to newPlace variable
   const placeTemplate = document.querySelector("#place-template").content;
   const newPlace = placeTemplate.querySelector(".element").cloneNode(true);
-
+  // Create event listener for cloned close button
   const deleteButton = newPlace.querySelector(".element__delete");
   deleteButton.addEventListener("click", (e) =>
     e.target.parentElement.remove()
   );
-
+  // Create event listener for cloned like button
   const likeButton = newPlace.querySelector(".element__heart");
   likeButton.addEventListener("click", (e) =>
     e.target.classList.toggle("element__heart_liked")
   );
-
+  // Create event listener on cloned image
   const placeImage = newPlace.querySelector(".element__image");
   placeImage.addEventListener("click", openPreview);
-
+  // Update cloned content with argument values
   newPlace.querySelector(".element__name").textContent = title;
   newPlace.querySelector(".element__image").src = link;
   newPlace.querySelector(".element__image").alt = `${title}`;
-
+  // Prepend new place in the places container
   placesContainer.prepend(newPlace);
 }
 
+// Save custom place to page
 function savePlace(e) {
   e.preventDefault();
-
+  // Add place using form input values
   addPlace(popupImageTitle.value, popupImageLink.value);
-  closeAdder();
+  closePopup();
 }
 
+// Save updated profile form
 function saveProfile(e) {
   e.preventDefault();
-
+  // Update profile with form input values
   profileName.textContent = popupName.value;
   profileTitle.textContent = popupTitle.value;
-
+  // Update profile object with newly updated profile content
   updateProfile();
-  closeEditor();
+  closePopup();
 }
 
-function closeEditor() {
+// Universal popup closer for save, create, close, and escape buttons
+function closePopup() {
   profileEditor.classList.remove("popup_opened");
-}
-
-function closeAdder() {
   newPlaceAdder.classList.remove("popup_opened");
-}
-
-function closePreview() {
   imagePreview.classList.remove("popup_opened");
 }
 
+// open image preview popup
 function openPreview(e) {
+  // update popup nodes with target src and alt values
   popupImage.src = e.target.src;
   popupImage.alt = e.target.alt;
+  // Update figure caption with alt text value
   popupImageCaption.textContent = e.target.alt;
+  // Open updated popup
   imagePreview.classList.toggle("popup_opened");
 }
 
 // event listeners
 editButton.addEventListener("click", editProfile);
 addButton.addEventListener("click", openPlaceAdder);
-closeProfileEditor.addEventListener("click", closeEditor);
-closeImageAdder.addEventListener("click", closeAdder);
-closePreviewButton.addEventListener("click", closePreview);
+closeProfileEditor.addEventListener("click", closePopup);
+closeImageAdder.addEventListener("click", closePopup);
+closePreviewButton.addEventListener("click", closePopup);
 profileEditorForm.addEventListener("submit", saveProfile);
 imageAdderForm.addEventListener("submit", savePlace);
