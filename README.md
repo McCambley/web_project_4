3f4463fe98be5c758ccea37308780d759f386a22
McCambley <jakemccamb16@gmail.com>
2021-04-19 00:16:56 -0400

    ref: Update readme and improve code readability

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Project 4: Around The U.S.
 
-
 ## Overview
 
 ### This project is hub for photos collected during a user's trip **Around the World**! In this case, these photos document my personal thru-hiking journey along the [Appalachian Trail](https://en.wikipedia.org/wiki/Appalachian_Trail). Currently, this hub allows a user to update their profile and like photos. They can update their name and title via a popup form and they can like photos by clicking the hearts displayed below each photo. Unfortunately, all of this updated information will reset after page refresh, but we're working on that.
@@ -9,24 +8,28 @@
 
 ## Technologies Used
 
-* **Figma**
-  * The layout for this project is based off of a Figma specification sheet designed by another [Practicum by Yandex](https://practicum.yandex.com/). Figma allows designers to put communicate their visions to developers without having to worry about how it will be coded.
-  * This design dictates how the page should appear to desktop users and mobile users. Designs for both the how the page upon initial load, as well as the page with the profile editor opened, should appear are displayed. [Check out the design here.](https://www.figma.com/file/SurN1jaeEQIhuZEDMhmWWf/Sprint-4-Around-The-U.S.-desktop-mobile?node-id=0%3A1)
+- **Figma**
 
-* **Media Queries**
-  * In order to create a page that responds to the viewport size through which the user views the page, [Media Queries](https://developer.mozilla.org/en-US/docs/Web/CSS/Media_Queries/Using_media_queries) were put to use. Users viewing the page on screens of all sizes (large desktops, average laptops, iPads, smartphones etc.) will each view a page designed for their individual experience.
+  - The layout for this project is based off of a Figma specification sheet designed by another [Practicum by Yandex](https://practicum.yandex.com/). Figma allows designers to put communicate their visions to developers without having to worry about how it will be coded.
+  - This design dictates how the page should appear to desktop users and mobile users. Designs for both the how the page upon initial load, as well as the page with the profile editor opened, should appear are displayed. [Check out the design here.](https://www.figma.com/file/SurN1jaeEQIhuZEDMhmWWf/Sprint-4-Around-The-U.S.-desktop-mobile?node-id=0%3A1)
 
-* **Javascript**
-  * This is the first project that contains any Javascript, which is very exciting. Specifically, this project implements [Event Listeners](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener) to perform certain actions based on user events. Currently, this project will open an editor when the "edit" button is clicked, it will close the editor without saving if the "X" button or the "Escape" key is actioned when the editor is open, it will save the form values when the "Save" button or the "Enter" key is actioned when the editor is opened, and it will toggle the appearance of the heart when an individual heart is clicked. Also, An alert will appear if the user attempts to add a photo. This functionality will be implemented on a later date.
+- **Media Queries**
 
-* **Git**
-  * While this is invisible to the user, an important addition to this project was the use of a development branch with distinct feature branches implemented on initial page construction. When the page is live, work can still be done and committed in pieces without disturbing the live version of the page. Using branches in this way is real-world development, and the use of them on this individual project is practice for when we begin working on projects with others.
+  - In order to create a page that responds to the viewport size through which the user views the page, [Media Queries](https://developer.mozilla.org/en-US/docs/Web/CSS/Media_Queries/Using_media_queries) were put to use. Users viewing the page on screens of all sizes (large desktops, average laptops, iPads, smartphones etc.) will each view a page designed for their individual experience.
+
+- **Javascript**
+
+  - This is the first project that contains any Javascript, which is very exciting. Specifically, this project implements [Event Listeners](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener) to perform certain actions based on user events. Currently, this project will open an editor when the "edit" button is clicked, it will close the editor without saving if the "X" button or the "Escape" key is actioned when the editor is open, it will save the form values when the "Save" button or the "Enter" key is actioned when the editor is opened, and it will toggle the appearance of the heart when an individual heart is clicked. Also, An alert will appear if the user attempts to add a photo. This functionality will be implemented on a later date.
+  - Recent update adds functionality to the "+" button. Users may now input their own photos from external urls. The title added to the post will automatically set the alt attibute of the image as the value of the title inpue.
+  - Users may now examine each photo in its original aspect ratio by clicked on an image to enlarge it.
+  - "Like" and "Delete" functionality has been updated to allow users to interact with both initial posts and subsequently updated posts. In future releases, the state of each "Like" and "Delete" will remain after page refresh. Currently, all input data is erased on page refresh.
+
+- **Git**
+  - While this is invisible to the user, an important addition to this project was the use of a development branch with distinct feature branches implemented on initial page construction. When the page is live, work can still be done and committed in pieces without disturbing the live version of the page. Using branches in this way is real-world development, and the use of them on this individual project is practice for when we begin working on projects with others.
 
 ## Future Plans
 
-* Allow users to update their own photos, including their profile avatar.
-* Save information (edited profile, uploaded photo, like photo) into a database for future reference after page refresh.
-* Allow user authentication.
+- Save information (edited profile, uploaded photo, like photo) into a database for future reference after page refresh.
+- Allow user authentication.
 
 ## [Check out this project live!](https://mccambley.github.io/web_project_4/)
-
--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,32 @@
 // Body identifiers
-let body = document.querySelector(".page");
-let profileName = document.querySelector(".profile__name");
-let profileTitle = document.querySelector(".profile__title");
-let placesContainer = document.querySelector(".elements");
-
+const body = document.querySelector(".page");
+const profileName = document.querySelector(".profile__name");
+const profileTitle = document.querySelector(".profile__title");
+const placesContainer = document.querySelector(".elements");
 
 // Profile editor identifiers
-let editButton = document.querySelector(".profile__edit-button");
-let closeProfileEditor = document.querySelector(".popup__close_role_edit");
-let profileEditor = document.querySelector(".popup_role_edit");
-let profileEditorForm = document.querySelector(".popup__form_role_edit");
-let popupName = document.querySelector(".popup__input_role_name");
-let popupTitle = document.querySelector(".popup__input_role_title");
+const editButton = document.querySelector(".profile__edit-button");
+const closeProfileEditor = document.querySelector(".popup__close_role_edit");
+const profileEditor = document.querySelector(".popup_role_edit");
+const profileEditorForm = document.querySelector(".popup__form_role_edit");
+const popupName = document.querySelector(".popup__input_role_name");
+const popupTitle = document.querySelector(".popup__input_role_title");
 
 // Image adder identifiers
-let addButton = document.querySelector(".profile__add-button");
-let closeImageAdder = document.querySelector(".popup__close_role_add");
-let newPlaceAdder = document.querySelector(".popup_role_add");
-let imageAdderForm = document.querySelector(".popup__form_role_add");
-let popupImageTitle = document.querySelector(".popup__input_role_image-title");
-let popupImageLink = document.querySelector(".popup__input_role_image-link");
+const addButton = document.querySelector(".profile__add-button");
+const closeImageAdder = document.querySelector(".popup__close_role_add");
+const newPlaceAdder = document.querySelector(".popup_role_add");
+const imageAdderForm = document.querySelector(".popup__form_role_add");
+const popupImageTitle = document.querySelector(
+  ".popup__input_role_image-title"
+);
+const popupImageLink = document.querySelector(".popup__input_role_image-link");
 
 // Image preview identifier
-let imagePreview = document.querySelector(".popup_role_image");
-let closePreviewButton = document.querySelector(".popup__close_role_image");
-let popupImage = document.querySelector(".popup__image")
-let popupImageCaption = document.querySelector(".popup__caption")
-
+const imagePreview = document.querySelector(".popup_role_image");
+const closePreviewButton = document.querySelector(".popup__close_role_image");
+const popupImage = document.querySelector(".popup__image");
+const popupImageCaption = document.querySelector(".popup__caption");
 
 // Retrieve profile object on page load
 let profile = {};
@@ -36,34 +36,33 @@ updateProfile();
 const initialCards = [
   {
     name: "Georgia",
-    link: "./images/franklin.jpeg"
+    link: "./images/franklin.jpeg",
   },
   {
     name: "Pisgah National Forest",
-    link: "./images/pisgah.jpeg"
+    link: "./images/pisgah.jpeg",
   },
   {
     name: "Dragon's Tooth",
-    link: "./images/dragons-tooth.jpeg"
+    link: "./images/dragons-tooth.jpeg",
   },
   {
     name: "Pennsylvania",
-    link: "./images/penn2.jpeg"
+    link: "./images/penn2.jpeg",
   },
   {
     name: "Stratton Pond",
-    link: "./images/stratton.jpeg"
+    link: "./images/stratton.jpeg",
   },
   {
     name: "Franconia Ridge",
-    link: "./images/franconia-ridge.jpeg"
-  }
+    link: "./images/franconia-ridge.jpeg",
+  },
 ];
 
-initialCards.forEach(place => {
+initialCards.forEach((place) => {
   addPlace(place.name, place.link);
-  // console.log("new place babyy");
-})
+});
 
 // functions
 function updateProfile() {
@@ -71,14 +70,10 @@ function updateProfile() {
     name: profileName.textContent,
     title: profileTitle.textContent,
   };
-  // console.log("profile object updated")
 }
-
-
 
 function editProfile() {
   updateProfile();
-  // console.log("Editing profile!");
   popupName.value = profile.name;
   popupTitle.value = profile.title;
   profileEditor.classList.add("popup_opened");
@@ -103,21 +98,25 @@ function openPlaceAdder() {
 }
 
 function addPlace(title, link) {
-  const placeTemplate = document.querySelector('#place-template').content;
-  const newPlace = placeTemplate.querySelector('.element').cloneNode(true);
+  const placeTemplate = document.querySelector("#place-template").content;
+  const newPlace = placeTemplate.querySelector(".element").cloneNode(true);
 
   const deleteButton = newPlace.querySelector(".element__delete");
-  deleteButton.addEventListener('click', e => e.target.parentElement.remove());
+  deleteButton.addEventListener("click", (e) =>
+    e.target.parentElement.remove()
+  );
 
   const likeButton = newPlace.querySelector(".element__heart");
-  likeButton.addEventListener('click', e => e.target.classList.toggle("element__heart_liked"));
+  likeButton.addEventListener("click", (e) =>
+    e.target.classList.toggle("element__heart_liked")
+  );
 
   const placeImage = newPlace.querySelector(".element__image");
-  placeImage.addEventListener('click', openPreview);
+  placeImage.addEventListener("click", openPreview);
 
-  newPlace.querySelector('.element__name').textContent = title;
-  newPlace.querySelector('.element__image').src = link;
-  newPlace.querySelector('.element__image').alt = `${title}`;
+  newPlace.querySelector(".element__name").textContent = title;
+  newPlace.querySelector(".element__image").src = link;
+  newPlace.querySelector(".element__image").alt = `${title}`;
 
   placesContainer.prepend(newPlace);
 }
@@ -140,65 +139,29 @@ function saveProfile(e) {
 }
 
 function closeEditor() {
-  // console.log("Closing Editor");
   profileEditor.classList.remove("popup_opened");
 }
 
 function closeAdder() {
-  // console.log("Closing Editor");
   newPlaceAdder.classList.remove("popup_opened");
 }
 
 function closePreview() {
-  // console.log("Closing preview");
   imagePreview.classList.remove("popup_opened");
 }
 
 function openPreview(e) {
-  popupImage.src = e.target.src
-  popupImage.alt = e.target.alt
-  popupImageCaption.textContent = e.target.alt
+  popupImage.src = e.target.src;
+  popupImage.alt = e.target.alt;
+  popupImageCaption.textContent = e.target.alt;
   imagePreview.classList.toggle("popup_opened");
 }
 
 // event listeners
 editButton.addEventListener("click", editProfile);
-addButton.addEventListener('click', openPlaceAdder)
+addButton.addEventListener("click", openPlaceAdder);
 closeProfileEditor.addEventListener("click", closeEditor);
 closeImageAdder.addEventListener("click", closeAdder);
 closePreviewButton.addEventListener("click", closePreview);
 profileEditorForm.addEventListener("submit", saveProfile);
 imageAdderForm.addEventListener("submit", savePlace);
-
-// Variables for later iterations
-
-
-// let saveButton = document.querySelector(".popup__save-button");
-
-// let heart = document.querySelector(".element__heart");
-
-// let allHearts = document.querySelectorAll(".element__heart");
-
-// Functions for later iterations
-
-// function addImage() {
-//   // console.log("The people really want this feature");
-//   alert("Currently working on this functionality! For now, enjoy these pretty pictures!");
-// }
-
-// function likeImage() {
-//   console.log("Liked!");
-//   document.querySelector(".element__heart").classList.toggle("element__heart_liked");
-// }
-
-// for (let i = 0; i < allHearts.length; i++) {
-//   let currentHeart = allHearts[i];
-//   currentHeart.addEventListener('click', () => {
-//     currentHeart.classList.toggle("element__heart_liked");
-//   });
-// }
-
-// Event listeners for later iterations
-
-// heart.addEventListener('click', likeImage);
-// addButton.addEventListener('click', addImage);
