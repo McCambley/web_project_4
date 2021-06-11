545fbca6a9c4ea2c2bd7590988ea2cf7820ad4b6
McCambley <jakemccamb16@gmail.com>
2021-06-11 12:02:04 -0400

    Ref: css minification install

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,40 @@
-// connect path, an OS agnostic filepath utility
+// webpack.config.js
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   devtool: "inline-source-map",
   entry: {
-    main: "./src/index.js", // this is the default js entry point
+    main: "./src/index.js",
   },
   output: {
-    // describes path to output file main.js
-    path: path.resolve(__dirname, "dist"), // creates path to dist/ directory
+    path: path.resolve(__dirname, "dist"),
     filename: "main.js",
     publicPath: "",
   },
-  target: ["web", "es5"], // ensure the Webpack glue code is ES5 compatible too
-  mode: "development", // setup for dev mode follows
+  target: ["web", "es5"],
+  stats: { children: true },
+  mode: "development",
   devServer: {
-    contentBase: path.resolve(__dirname, "./dist"), // convert relative path to absolute
-    compress: true, // this will speed up file loading in development mode
-    port: 8080, // will open your site at localhost:8080
-    open: true, // site will open automatically in the browser
+    contentBase: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 8080,
+    open: true,
   },
   module: {
     rules: [
-      // this is an array of rules
-      // add an object containing rules for Babel to it
       {
-        // a regular expression that searches for all js files
         test: /\.js$/,
-        // all files must be processed by babel-loader
         loader: "babel-loader",
-        // exclude the node_modules folder, we don't need to process files in it
         exclude: "/node_modules/",
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
